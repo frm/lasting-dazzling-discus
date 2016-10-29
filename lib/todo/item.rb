f15435faf4fdb2fc8e03907c987e481cb08df2c3
Fernando Mendes <devfrmendes@gmail.com>
2016-10-29 17:40:25 +0100

    Adds final todo list exercises

--- lib/todo/item.rb
+++ lib/todo/item.rb
@@ -1,15 +1,18 @@
 module Todo
   class Item
+    attr_reader :text
+
     def initialize(text)
+      @text = text
+      @status = :incomplete
     end
 
     def complete?
+      @status == :complete
     end
 
     def complete
-    end
-
-    def text
+      @status = :complete
     end
   end
 end
--- lib/todo/list.rb
+++ lib/todo/list.rb
@@ -2,36 +2,58 @@ require_relative "item"
 
 module Todo
   class List
+    @@complete = -> (id, todo) { todo.complete? }
+
+    # As a matter of personal taste, I would remove the @@complete
+    # And rewrite it like so:
+    # class << self
+    #   def complete
+    #     @_complete ||= -> (id, todo) { todo.complete? }
+    #   end
+    # end
+
     def initialize
       @list = {}
       @id = 0
     end
 
     def add(text)
+      @id += 1
+      @list[@id] = Todo::Item.new(text)
+
+      @id
     end
 
     def remove(id)
+      @list.delete(id)
     end
 
     def [](id)
+      @list[id]
     end
 
     def complete_todo(id)
+      @list[id].complete
     end
 
     def all
+      @list.values
     end
 
     def complete
+      @list.select(&@@complete).values
     end
 
     def incomplete
+      @list.reject(&@@complete).values
     end
 
     def prune
+      @list.delete_if(&@@complete)
     end
 
     def count
+      @list.length
     end
   end
 end
