3f5692406ed66f2d74328031722bfa670e79c3a4
Fernando Mendes <devfrmendes@gmail.com>
2016-10-29 17:10:57 +0100

    Sets up todo list

---
+++ lib/todo/list.rb
@@ -0,0 +1,37 @@
+require_relative "item"
+
+module Todo
+  class List
+    def initialize
+      @list = {}
+      @id = 0
+    end
+
+    def add(text)
+    end
+
+    def remove(id)
+    end
+
+    def [](id)
+    end
+
+    def complete_todo(id)
+    end
+
+    def all
+    end
+
+    def complete
+    end
+
+    def incomplete
+    end
+
+    def prune
+    end
+
+    def count
+    end
+  end
+end
